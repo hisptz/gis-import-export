04ab63d4366a18cfd87b5b90c7d25b386c9e6e5f
vmataba <vmataba0@gmail.com>
2019-02-26 23:31:22 +0300

    Update results.action.ts

--- src/app/store/actions/results.action.ts
+++ src/app/store/actions/results.action.ts
@@ -1,9 +1,10 @@
 import { Action } from '@ngrx/store';
-import { OrganisationUnit } from 'src/app/core';
+import { OrganisationUnit, MatchScheme,MatchResults } from 'src/app/core/models';
 
 export const enum ResultsActionTypes {
 	LOAD_AVAILABLE_ORG_UNITS = '[Results] Load available org units',
 	LOAD_FILE_CONTENTS = '[Results] Load file contents',
+	UPDATE_MATCH_SCHEME = '[Results] Update match scheme',
 	START_MATCH = '[Results] Start Match',
 	END_MATCH = '[Results] End Match'
 }
@@ -18,12 +19,18 @@ export class LoadFileContents implements Action {
 	constructor(public fileContents: any) {}
 }
 
+export class UpdateMatchScheme {
+	readonly type = ResultsActionTypes.UPDATE_MATCH_SCHEME;
+	constructor(public matchScheme: MatchScheme) {}
+}
+
 export class StartMatch implements Action {
 	readonly type = ResultsActionTypes.START_MATCH;
 }
 
 export class EndMatch implements Action {
 	readonly type = ResultsActionTypes.END_MATCH;
+	constructor(public matchResults: MatchResults){}
 }
 
-export type ResultsActions = LoadAvailableOrgUnits | LoadFileContents | StartMatch | EndMatch;
+export type ResultsActions = LoadAvailableOrgUnits | LoadFileContents | UpdateMatchScheme | StartMatch | EndMatch;
