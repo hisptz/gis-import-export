a0073b59b67d36d52877b3f287700f86126dc2bc
vmataba <vmataba0@gmail.com>
2019-02-26 14:25:17 +0300

    Add file.selector.ts

---
+++ src/app/store/selectors/file.selector.ts
@@ -0,0 +1,11 @@
+import { createSelector } from '@ngrx/store';
+import { getRootState, State } from '../reducers';
+import * as fromFileReducer from '../reducers/file.reducer';
+
+export const getFileState = createSelector(getRootState, (state: State) => state.file);
+
+export const getFileLoading = createSelector(getFileState, fromFileReducer.getLoading)
+export const getFileLoaded = createSelector(getFileState, fromFileReducer.getLoaded)
+export const getFileFailed = createSelector(getFileState, fromFileReducer.getFailed)
+export const getFileError = createSelector(getFileState,fromFileReducer.getError)
+export const getFileFileContents = createSelector(getFileState,fromFileReducer.getFileContents)
--- src/app/store/selectors/index.ts
+++ src/app/store/selectors/index.ts
@@ -2,3 +2,4 @@ export * from './router.selectors';
 export * from './system-info.selectors';
 export * from './user.selectors';
 export * from './org-unit.selector'
+export * from './file.selector'
