0f67fcba9765d5a4e5f98a7ae667227200178585
vmataba <vmataba0@gmail.com>
2019-02-26 23:32:17 +0300

    Update match-result.model.ts

--- src/app/core/models/match-result.model.ts
+++ src/app/core/models/match-result.model.ts
@@ -1,7 +1,13 @@
-import { OrganisationUnit } from './org-unit.model'
-import { Feature } from './feature.model'
+import { OrganisationUnit } from './org-unit.model';
+import { Feature } from './feature.model';
+import { ResultsSummary } from './results-summary.model';
 
-export interface Result { 
-    feature: Feature
-    organisationUnits:OrganisationUnit[]
+export interface Result {
+	feature: Feature;
+	organisationUnits: OrganisationUnit[];
+}
+
+export interface MatchResults {
+	summary: ResultsSummary;
+	results: Result[];
 }
