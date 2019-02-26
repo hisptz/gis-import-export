64311f175fa436fc56c424a35334b5075b02c210
vmataba <vmataba0@gmail.com>
2019-02-26 14:16:23 +0300

    Update reducers/index.ts

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -10,6 +10,7 @@ import { environment } from '../../../environments/environment';
 import * as fromUser from '../reducers/user.reducer';
 import * as fromSystemInfo from '../reducers/system-info.reducer';
 import * as fromOrgUnits from '../reducers/org-units.reducer';
+import * as fromFileReducer from '../reducers/file.reducer'
 import { RouterReducerState, routerReducer } from '@ngrx/router-store';
 
 export interface State {
@@ -29,13 +30,16 @@ export interface State {
   route: RouterReducerState;
   //Org Units state
   orgUnits: fromOrgUnits.OrgUnitsState
+  //File State
+  file: fromFileReducer.FileState
 }
 
 export const reducers: ActionReducerMap<State> = {
   user: fromUser.reducer,
   systemInfo: fromSystemInfo.reducer,
   route: routerReducer,
-  orgUnits: fromOrgUnits.reducer
+  orgUnits: fromOrgUnits.reducer,
+  file:fromFileReducer.reducer
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
