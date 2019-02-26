b54719465164aa167fb942d2c76cfdd217871c1d
vmataba <vmataba0@gmail.com>
2019-02-26 23:52:39 +0300

    Update reducers/index.st

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,50 +1,46 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 
-import * as fromUser from '../reducers/user.reducer';
-import * as fromSystemInfo from '../reducers/system-info.reducer';
-import * as fromOrgUnits from '../reducers/org-units.reducer';
-import * as fromFileReducer from '../reducers/file.reducer'
+import * as fromUserReducer from '../reducers/user.reducer';
+import * as fromSystemInfoReducer from '../reducers/system-info.reducer';
+import * as fromOrgUnitsReducer from '../reducers/org-units.reducer';
+import * as fromFileReducer from '../reducers/file.reducer';
+import * as fromResultsReducer from '../reducers/results.reducer';
 import { RouterReducerState, routerReducer } from '@ngrx/router-store';
 
 export interface State {
-  /**
+	/**
    * User state
    */
-  user: fromUser.State;
+	user: fromUserReducer.State;
 
-  /**
+	/**
    * System info state
    */
-  systemInfo: fromSystemInfo.State;
+	systemInfo: fromSystemInfoReducer.State;
 
-  /**
+	/**
    * Router state
    */
-  route: RouterReducerState;
-  //Org Units state
-  orgUnits: fromOrgUnits.OrgUnitsState
-  //File State
-  file: fromFileReducer.FileState
+	route: RouterReducerState;
+	//Org Units state
+	orgUnits: fromOrgUnitsReducer.OrgUnitsState;
+	//File State
+	file: fromFileReducer.FileState;
+	//Match Results
+	results: fromResultsReducer.ResultsState;
 }
 
 export const reducers: ActionReducerMap<State> = {
-  user: fromUser.reducer,
-  systemInfo: fromSystemInfo.reducer,
-  route: routerReducer,
-  orgUnits: fromOrgUnits.reducer,
-  file:fromFileReducer.reducer
+	user: fromUserReducer.reducer,
+	systemInfo: fromSystemInfoReducer.reducer,
+	route: routerReducer,
+	orgUnits: fromOrgUnitsReducer.reducer,
+	file: fromFileReducer.reducer,
+	results: fromResultsReducer.reducer
 };
 
-export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
-  : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
 
 /**
  * Root state selector
