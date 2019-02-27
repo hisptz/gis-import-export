588159343d9045f140e758bf4c06361486d28db5
vmataba <vmataba0@gmail.com>
2019-02-28 01:53:07 +0300

    Turn search function more generic

--- src/app/core/utils/search.util.ts
+++ src/app/core/utils/search.util.ts
@@ -1,13 +1,21 @@
 import * as _ from 'lodash';
 
-export function search(collection: any[], key: any, value: any, option = 'exact') {
+export function search(collection: any[], key: any, value: any, option = 'includes') {
+	if (value == '') {
+		return collection;
+	}
 	switch (option) {
 		case 'exact':
-			return _.filter(collection, (item) => item[key] == value);
-		case 'include':
-			return _.filter(collection, (item) => value in item[key]);
+			return _.filter(collection, (item) => item[key].toString().toLowerCase() == value.toString().toLowerCase());
+		case 'includes':
+			return _.filter(collection, (item) =>
+				item[key].toString().toLowerCase().includes(value.toString().toLowerCase())
+			);
 		case 'exclude':
-			return _.filter(collection, (item) => !(value in item[key]));
+			return _.filter(
+				collection,
+				(item) => !item[key].toString().toLowerCase().includes(value.toString().toLowerCase())
+			);
 		default:
 			return collection;
 	}
